97623b8ed4485c32fc71c9f80c6ebeb8b24a6fbc
Htun Aung Kyaw <htunaungkyaw730@gmail.com>
2020-06-27 19:56:32 +0630

    new four codes

--- domcrashcourse/dom.js
+++ domcrashcourse/dom.js
@@ -5,10 +5,10 @@
 // console.log(document.URL);
 // console.log(document.title);
 // //document.title =  123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
+ console.log(document.doctype);
+ console.log(document.head);
+ console.log(document.body);
+ console.log(document.all);
 // console.log(document.all[10]);
 // // document.all[10].textContent = 'Hello';
 // console.log(document.forms[0]);

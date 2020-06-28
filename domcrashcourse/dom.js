bd06fadf23763b529e93eda883111c3cb08526a4
Htun Aung Kyaw <htunaungkyaw730@gmail.com>
2020-06-28 13:37:48 +0630

    New Changes DOM

--- domcrashcourse/dom.js
+++ domcrashcourse/dom.js
@@ -1,242 +1,242 @@
-// EXAMINE THE DOCUMENT OBJECT //
-
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// //document.title =  123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[10]);
-// // document.all[10].textContent = 'Hello';
-// console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
-
-// GETELEMENTBYID //
-// console.log(document.getElementById('header-title'));
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-// console.log(headerTitle);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = 'Goodbye';
-// console.log(headerTitle.innerText);
-// headerTitle.innerHTML = '<h3>Hello</h3>';
-// header.style.borderBottom = 'solid 3px #000';
-
-// GETELEMENTSBYCLASSNAME //
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.fontWeight = 'bold';
-// items[1].style.backgroundColor = 'yellow';
-
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
-
-// for(var i = 0; i < items.length; i++){
-//   items[i].style.backgroundColor = '#f4f4f4';
-// }
-
-// GETELEMENTSBYTAGNAME //
-// var li = document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-// li[1].textContent = 'Hello 2';
-// li[1].style.fontWeight = 'bold';
-// li[1].style.backgroundColor = 'yellow';
-
-// // Gives error
-// //items.style.backgroundColor = '#f4f4f4';
-
-// for(var i = 0; i < li.length; i++){
-//   li[i].style.backgroundColor = '#f4f4f4';
-// }
-
-// QUERYSELECTOR //
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom = 'solid 4px #ccc';
-
-// var input = document.querySelector('input');
-// input.value = 'Hello World'
+//EXAMINE THE DOCUMENT OBJECT //
+
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+//document.title =  123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+
+//GETELEMENTBYID //
+console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+headerTitle.innerText = 'Goodbye';
+console.log(headerTitle.innerText);
+headerTitle.innerHTML = '<h3>Hello</h3>';
+header.style.borderBottom = 'solid 3px #000';
+
+//GETELEMENTSBYCLASSNAME //
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < items.length; i++){
+  items[i].style.backgroundColor = '#f4f4f4';
+}
+
+//GETELEMENTSBYTAGNAME //
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < li.length; i++){
+  li[i].style.backgroundColor = '#f4f4f4';
+}
+
+//QUERYSELECTOR //
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World'
 
-// var submit = document.querySelector('input[type="submit"]');
-// submit.value="SEND"
+var submit = document.querySelector('input[type="submit"]');
+submit.value="SEND"
 
-// var item = document.querySelector('.list-group-item');
-// item.style.color = 'red';
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
 
-// var lastItem = document.querySelector('.list-group-item:last-child');
-// lastItem.style.color = 'blue';
-
-// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.color = 'coral';
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+
+//QUERYSELECTORALL //
+var titles = document.querySelectorAll('.title');
 
-// QUERYSELECTORALL //
-// var titles = document.querySelectorAll('.title');
-
-// console.log(titles);
-// titles[0].textContent = 'Hello';
-
-// var odd = document.querySelectorAll('li:nth-child(odd)');
-// var even= document.querySelectorAll('li:nth-child(even)');
-
-// for(var i = 0; i < odd.length; i++){
-//   odd[i].style.backgroundColor = '#f4f4f4';
-//   even[i].style.backgroundColor = '#ccc';
-// }
+console.log(titles);
+titles[0].textContent = 'Hello';
 
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even= document.querySelectorAll('li:nth-child(even)');
 
+for(var i = 0; i < odd.length; i++){
+  odd[i].style.backgroundColor = '#f4f4f4';
+  even[i].style.backgroundColor = '#ccc';
+}
 
-// TRAVERSING THE DOM //
-//var itemList = document.querySelector('#items');
-// parentNode
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentNode.parentNode.parentNode);
 
-// parentElement
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentElement.parentElement.parentElement);
 
-// childNodes
-// console.log(itemList.childNodes);
+//TRAVERSING THE DOM //
+var itemList = document.querySelector('#items');
+//parentNode
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode.parentNode);
 
-// console.log(itemList.children);
-// console.log(itemList.children[1]);
-// itemList.children[1].style.backgroundColor = 'yellow';
+//parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
 
-// // FirstChild
-// console.log(itemList.firstChild);
-// // firstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent = 'Hello 1';
+//childNodes
+console.log(itemList.childNodes);
 
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor = 'yellow';
 
-// lastChild
-// console.log(itemList.lastChild);
-// lastElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent = 'Hello 4';
+// FirstChild
+console.log(itemList.firstChild);
+// firstElementChild
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'Hello 1';
 
-// nextSibling
-// console.log(itemList.nextSibling);
-// // nextElementSibling
-// console.log(itemList.nextElementSibling);
 
-// previousSibling
-// console.log(itemList.previousSibling);
-// previousElementSibling
-// console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
+//lastChild
+console.log(itemList.lastChild);
+//lastElementChild
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Hello 4';
 
-// createElement
+//nextSibling
+console.log(itemList.nextSibling);
+// nextElementSibling
+console.log(itemList.nextElementSibling);
 
-// // Create a div
-// var newDiv =  document.createElement('div');
+//previousSibling
+console.log(itemList.previousSibling);
+//previousElementSibling
+console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
 
-// // Add class
-// newDiv.className= 'hello';
+//createElement
 
-// // Add id
-// newDiv.id = 'hello1';
+// Create a div
+var newDiv =  document.createElement('div');
 
-// // Add attr
-// newDiv.setAttribute('title', 'Hello Div');
+// Add class
+newDiv.className= 'hello';
 
-// // Create text node
-// var newDivText = document.createTextNode('Hello World');
+// Add id
+newDiv.id = 'hello1';
 
-// // Add text to div
-// newDiv.appendChild(newDivText);
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
 
-// var container = document.querySelector('header .container');
-// var h1 = document.querySelector('header h1');
+// Create text node
+var newDivText = document.createTextNode('Hello World');
 
-// console.log(newDiv);
+// Add text to div
+newDiv.appendChild(newDivText);
 
-// newDiv.style.fontSize = '30px';
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
 
-// container.insertBefore(newDiv, h1);
+console.log(newDiv);
 
-// EVENTS //
+newDiv.style.fontSize = '30px';
 
-// var button = document.getElementById('button').addEventListener('click', buttonClick);
+container.insertBefore(newDiv, h1);
 
-// function buttonClick(e){
-//   //console.log('Button clicked');
-//   // document.getElementById('header-title').textContent = 'Changed';
-//   // document.querySelector('#main').style.backgroundColor = '#f4f4f4';
-//   //console.log(e);
+//EVENTS //
 
-//   // console.log(e.target);
-//   // console.log(e.target.id);
-//   // console.log(e.target.className);
-//   // console.log(e.target.classList);
-//   // var output = document.getElementById('output');
-//   // output.innerHTML = '<h3>'+e.target.id+'</h3>';
+var button = document.getElementById('button').addEventListener('click', buttonClick);
 
-//   // console.log(e.type);
+function buttonClick(e){
+  console.log('Button clicked');
+  document.getElementById('header-title').textContent = 'Changed';
+  document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+  console.log(e);
+  console.log(e.target);
+  console.log(e.target.id);
+  console.log(e.target.className);
+  console.log(e.target.classList);
 
-//   //console.log(e.clientX);
-//   //console.log(e.clientY);
 
-//   //console.log(e.offsetX);
-//   //console.log(e.offsetY);
+  var output = document.getElementById('output');
+  output.innerHTML = '<h3>'+e.target.id+'</h3>';
 
-//   // console.log(e.altKey);
-//   // console.log(e.ctrlKey);
-//   // console.log(e.shiftKey);
-// }
+ console.log(e.type);
 
-// var button = document.getElementById('button');
-// var box = document.getElementById('box');
+  console.log(e.clientX);
+  console.log(e.clientY);
 
-//button.addEventListener('click', runEvent);
-//button.addEventListener('dblclick', runEvent);
-//button.addEventListener('mousedown', runEvent);
-//button.addEventListener('mouseup', runEvent);
+  console.log(e.offsetX);
+  console.log(e.offsetY);
 
-//box.addEventListener('mouseenter', runEvent);
-//box.addEventListener('mouseleave', runEvent);
+  console.log(e.altKey);
+  console.log(e.ctrlKey);
+  console.log(e.shiftKey);
+}
+var button = document.getElementById('button');
+var box = document.getElementById('box');
 
-//box.addEventListener('mouseover', runEvent);
-//box.addEventListener('mouseout', runEvent);
+button.addEventListener('click', runEvent);
+button.addEventListener('dblclick', runEvent);
+button.addEventListener('mousedown', runEvent);
+button.addEventListener('mouseup', runEvent);
 
-// box.addEventListener('mousemove', runEvent);
+box.addEventListener('mouseenter', runEvent);
+box.addEventListener('mouseleave', runEvent);
 
-// var itemInput = document.querySelector('input[type="text"]');
-// var form = document.querySelector('form');
-// var select = document.querySelector('select');
+box.addEventListener('mouseover', runEvent);
+box.addEventListener('mouseout', runEvent);
 
-// itemInput.addEventListener('keydown', runEvent);
-// itemInput.addEventListener('keyup', runEvent);
-// itemInput.addEventListener('keypress', runEvent);
+ box.addEventListener('mousemove', runEvent);
 
-// itemInput.addEventListener('focus', runEvent);
-// itemInput.addEventListener('blur', runEvent);
+ var itemInput = document.querySelector('input[type="text"]');
+ var form = document.querySelector('form');
+ var select = document.querySelector('select');
 
-// itemInput.addEventListener('cut', runEvent);
-// itemInput.addEventListener('paste', runEvent);
+ itemInput.addEventListener('keydown', runEvent);
+itemInput.addEventListener('keyup', runEvent);
+ itemInput.addEventListener('keypress', runEvent);
 
-// itemInput.addEventListener('input', runEvent);
+itemInput.addEventListener('focus', runEvent);
+ itemInput.addEventListener('blur', runEvent);
 
-// select.addEventListener('change', runEvent);
-// select.addEventListener('input', runEvent);
+itemInput.addEventListener('cut', runEvent);
+ itemInput.addEventListener('paste', runEvent);
 
-// form.addEventListener('submit', runEvent);
+ itemInput.addEventListener('input', runEvent);
 
-// function runEvent(e){
-//   e.preventDefault();
-//   console.log('EVENT TYPE: '+e.type);
+ select.addEventListener('change', runEvent);
+ select.addEventListener('input', runEvent);
 
-  //console.log(e.target.value);
-  // document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3>';
+ form.addEventListener('submit', runEvent);
 
-  // output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>';
+function runEvent(e){
+  e.preventDefault();
+  console.log('EVENT TYPE: '+e.type);
 
-  // document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)";
-//}
+   console.log(e.target.value);
+   document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3>';
+
+   output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>';
+
+   document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)";
+}

daf57c363d39eaedb0b08496f62ef3d19809e033
Htun Aung Kyaw <htunaungkyaw730@gmail.com>
2020-06-28 11:32:10 +0630

    new changes 3

--- domcrashcourse/dom.js
+++ domcrashcourse/dom.js
@@ -5,10 +5,10 @@
 // console.log(document.URL);
 // console.log(document.title);
 // //document.title =  123;
-//console.log(document.doctype);
-//console.log(document.head);
-//console.log(document.body);
-//console.log(document.all);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
 // console.log(document.all[10]);
 // // document.all[10].textContent = 'Hello';
 // console.log(document.forms[0]);
@@ -92,12 +92,13 @@
 
 
 // TRAVERSING THE DOM //
-var itemList = document.querySelector('#items');
+//var itemList = document.querySelector('#items');
 // parentNode
 // console.log(itemList.parentNode);
 // itemList.parentNode.style.backgroundColor = '#f4f4f4';
 // console.log(itemList.parentNode.parentNode.parentNode);
-//parentElement  
+
+// parentElement
 // console.log(itemList.parentElement);
 // itemList.parentElement.style.backgroundColor = '#f4f4f4';
 // console.log(itemList.parentElement.parentElement.parentElement);
@@ -109,7 +110,7 @@ var itemList = document.querySelector('#items');
 // console.log(itemList.children[1]);
 // itemList.children[1].style.backgroundColor = 'yellow';
 
-// // // FirstChild
+// // FirstChild
 // console.log(itemList.firstChild);
 // // firstElementChild
 // console.log(itemList.firstElementChild);
@@ -118,27 +119,27 @@ var itemList = document.querySelector('#items');
 
 // lastChild
 // console.log(itemList.lastChild);
-// //lastElementChild
+// lastElementChild
 // console.log(itemList.lastElementChild);
 // itemList.lastElementChild.textContent = 'Hello 4';
 
 // nextSibling
 // console.log(itemList.nextSibling);
-// // // nextElementSibling
+// // nextElementSibling
 // console.log(itemList.nextElementSibling);
 
 // previousSibling
-console.log(itemList.previousSibling);
+// console.log(itemList.previousSibling);
 // previousElementSibling
-console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
+// console.log(itemList.previousElementSibling);itemList.previousElementSibling.style.color = 'green';
 
 // createElement
 
-// Create a div
-// var newDiv = document.createElement('div');
+// // Create a div
+// var newDiv =  document.createElement('div');
 
 // // Add class
-// // newDiv.className = 'hello';
+// newDiv.className= 'hello';
 
 // // Add id
 // newDiv.id = 'hello1';
@@ -160,3 +161,82 @@ console.log(itemList.previousElementSibling);itemList.previousElementSibling.sty
 // newDiv.style.fontSize = '30px';
 
 // container.insertBefore(newDiv, h1);
+
+// EVENTS //
+
+// var button = document.getElementById('button').addEventListener('click', buttonClick);
+
+// function buttonClick(e){
+//   //console.log('Button clicked');
+//   // document.getElementById('header-title').textContent = 'Changed';
+//   // document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+//   //console.log(e);
+
+//   // console.log(e.target);
+//   // console.log(e.target.id);
+//   // console.log(e.target.className);
+//   // console.log(e.target.classList);
+//   // var output = document.getElementById('output');
+//   // output.innerHTML = '<h3>'+e.target.id+'</h3>';
+
+//   // console.log(e.type);
+
+//   //console.log(e.clientX);
+//   //console.log(e.clientY);
+
+//   //console.log(e.offsetX);
+//   //console.log(e.offsetY);
+
+//   // console.log(e.altKey);
+//   // console.log(e.ctrlKey);
+//   // console.log(e.shiftKey);
+// }
+
+// var button = document.getElementById('button');
+// var box = document.getElementById('box');
+
+//button.addEventListener('click', runEvent);
+//button.addEventListener('dblclick', runEvent);
+//button.addEventListener('mousedown', runEvent);
+//button.addEventListener('mouseup', runEvent);
+
+//box.addEventListener('mouseenter', runEvent);
+//box.addEventListener('mouseleave', runEvent);
+
+//box.addEventListener('mouseover', runEvent);
+//box.addEventListener('mouseout', runEvent);
+
+// box.addEventListener('mousemove', runEvent);
+
+// var itemInput = document.querySelector('input[type="text"]');
+// var form = document.querySelector('form');
+// var select = document.querySelector('select');
+
+// itemInput.addEventListener('keydown', runEvent);
+// itemInput.addEventListener('keyup', runEvent);
+// itemInput.addEventListener('keypress', runEvent);
+
+// itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('blur', runEvent);
+
+// itemInput.addEventListener('cut', runEvent);
+// itemInput.addEventListener('paste', runEvent);
+
+// itemInput.addEventListener('input', runEvent);
+
+// select.addEventListener('change', runEvent);
+// select.addEventListener('input', runEvent);
+
+// form.addEventListener('submit', runEvent);
+
+// function runEvent(e){
+//   e.preventDefault();
+//   console.log('EVENT TYPE: '+e.type);
+
+  //console.log(e.target.value);
+  // document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3>';
+
+  // output.innerHTML = '<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>';
+
+  // document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 40)";
+//}
